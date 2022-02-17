29b27f8fc7e4087a631e8caf4a0b01250031ccce
Hex <35177464+HexCodeFFF@users.noreply.github.com>
2022-02-17 15:14:22 -0600

    hello worl

---
+++ app.js
@@ -0,0 +1,33 @@
+function fail(str) {
+    console.log(str)
+    process.exit(1)
+}
+
+const minimist = require('minimist')
+const SamJs = require("sam-js")
+const fs = require("fs")
+const path = require("path");
+// parse args
+let argv = minimist(process.argv.slice(2), {boolean:["moderncmu", "debug"]})
+if (!("wav" in argv)) {
+    fail("wav output unspecified!")
+}
+if (!argv._.length) {
+    fail("speak string unspecified!")
+}
+// suppress output if not in debug mode
+if (!argv.debug) {
+    console["log"] = () => {
+    }
+    console["debug"] = () => {
+    }
+    console["info"] = () => {
+    }
+}
+const tospeak = argv._.join(" ")
+console.log(tospeak);
+// run sammy wammy
+let sam = new SamJs(argv);
+let buf = sam.renderwav(tospeak);
+// write to file
+fs.writeFileSync(argv["wav"], Buffer.from(buf))
